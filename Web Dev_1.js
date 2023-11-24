cb581cba89e8cd5a196be3cd8bee38770a034412
Aryan <110730264+123Aryan456@users.noreply.github.com>
2023-11-24 17:10:09 +0530

    Add files via upload

---
+++ Web Dev_1.js
@@ -0,0 +1,39 @@
+//locomotive js - smooth scrolling
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+//gsap to animate
+gsap.from(".nlink",{
+    stagger: .2,
+    y: 10,
+    duration: 1,
+    ease: Power2,
+    opacity: 0,
+
+})
+Shery.textAnimate("#headings h1", {
+    style: 2,
+    y: 10,
+    delay: 0.3,
+    duration: 3,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    multiplier: 0.1,
+
+  });
+
+gsap.from(".anim2",{
+    y: 50,
+    stagger: .3,
+    opacity: 0,
+    ease: Expo,
+    duration: 0.4,
+})  
+
+//Shery.imageEffect("#imgntexts img", {
+  //  style: 4,
+  //  debug: true,
+//})
+
+//shery js to animate images
