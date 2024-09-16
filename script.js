3ee207dde428d32368ecace9dff505be1d2a42ea
Aryan <110730264+123Aryan456@users.noreply.github.com>
2024-09-16 18:25:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,6 +31,36 @@ gsap.from(".anim2",{
     duration: 0.4,
 })  
 
+
+var myHeaders = new Headers();
+myHeaders.append("Authorization", "Basic {ACCESS_TOKEN}");
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "start_node_id": "axi1-start",
+  "end_node_id": "AQ6K-end",
+  "result_node_id": "AQ6K-end",
+  "node_settings": {
+    "axi1-start": {
+      "data": {
+        "1": {STRING}
+      }
+    }
+  }
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("https://api.scade.pro/api/v1/scade/flow/37375/execute", requestOptions)
+  .then(response => response.json())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
 //Shery.imageEffect("#imgntexts img", {
     //style: 4,
     //debug: true,
